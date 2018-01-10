7c6ee2b951f242da01a9996bf8c8531722c24986
DESKTOP-IFFMODA\Jay <xiajie0929@163.com>
2018-01-10 23:25:45 +0800

    初步提交mvp模式

---
+++ mvp/src/main/java/com/jay/mvp/integration/FragmentLifecycle.java
@@ -0,0 +1,102 @@
+/**
+  * Copyright 2017 JessYan
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package com.jay.mvp.integration;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+
+import com.jay.mvp.base.delegate.IFragment;
+
+import timber.log.Timber;
+
+/**
+ * ================================================
+ * {@link FragmentManager.FragmentLifecycleCallbacks} 默认实现类
+ * <p>
+ * Created by JessYan on 04/09/2017 16:04
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public class FragmentLifecycle extends FragmentManager.FragmentLifecycleCallbacks {
+
+    @Override
+    public void onFragmentAttached(FragmentManager fm, Fragment f, Context context) {
+        Timber.w(f.toString() + " - onFragmentAttached");
+        if (f instanceof IFragment) {
+
+        }
+    }
+
+    @Override
+    public void onFragmentCreated(FragmentManager fm, Fragment f, Bundle savedInstanceState) {
+        Timber.w(f.toString() + " - onFragmentCreated");
+    }
+
+    @Override
+    public void onFragmentViewCreated(FragmentManager fm, Fragment f, View v, Bundle savedInstanceState) {
+        Timber.w(f.toString() + " - onFragmentViewCreated");
+    }
+
+    @Override
+    public void onFragmentActivityCreated(FragmentManager fm, Fragment f, Bundle savedInstanceState) {
+        Timber.w(f.toString() + " - onFragmentActivityCreated");
+    }
+
+    @Override
+    public void onFragmentStarted(FragmentManager fm, Fragment f) {
+        Timber.w(f.toString() + " - onFragmentStarted");
+    }
+
+    @Override
+    public void onFragmentResumed(FragmentManager fm, Fragment f) {
+        Timber.w(f.toString() + " - onFragmentResumed");
+    }
+
+    @Override
+    public void onFragmentPaused(FragmentManager fm, Fragment f) {
+        Timber.w(f.toString() + " - onFragmentPaused");
+    }
+
+    @Override
+    public void onFragmentStopped(FragmentManager fm, Fragment f) {
+        Timber.w(f.toString() + " - onFragmentStopped");
+    }
+
+    @Override
+    public void onFragmentSaveInstanceState(FragmentManager fm, Fragment f, Bundle outState) {
+        Timber.w(f.toString() + " - onFragmentSaveInstanceState");
+    }
+
+    @Override
+    public void onFragmentViewDestroyed(FragmentManager fm, Fragment f) {
+        Timber.w(f.toString() + " - onFragmentViewDestroyed");
+    }
+
+    @Override
+    public void onFragmentDestroyed(FragmentManager fm, Fragment f) {
+        Timber.w(f.toString() + " - onFragmentDestroyed");
+    }
+
+    @Override
+    public void onFragmentDetached(FragmentManager fm, Fragment f) {
+        Timber.w(f.toString() + " - onFragmentDetached");
+    }
+
+}
